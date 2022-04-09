b727f2b200c9b5739da0050f63678c93c1966bbc
dyoshikawa <yoshikawa.daio@classmethod.jp>
2022-04-09 13:34:25 +0900

    todo-api

---
+++ packages/todo-api/src/main.ts
@@ -0,0 +1,64 @@
+import express from 'express'
+import { v4 as uuidV4 } from 'uuid'
+import fs from 'fs'
+
+const app = express()
+
+app.use(express.json())
+
+interface Todo {
+  id: string
+  content: string
+}
+
+app.get('/todos', async (_, res) => {
+  if (!fs.existsSync('./db.json')) {
+    res.json({ todos: [] })
+    return
+  }
+
+  const todos: Todo[] = JSON.parse(fs.readFileSync('./db.json').toString())
+
+  res.json({ todos })
+})
+
+type CreateTodoParams = Pick<Todo, 'content'>
+
+app.post('/todos', async (req, res) => {
+  const params: CreateTodoParams = req.body
+  const todo: Todo = { ...params, id: uuidV4() }
+
+  if (!fs.existsSync('./db.json')) {
+    fs.writeFileSync('./db.json', JSON.stringify([todo], null, 2))
+    res.json({ message: 'Success!' })
+
+    return
+  }
+
+  const todos: Todo[] = JSON.parse(fs.readFileSync('./db.json').toString())
+  const newTodos: Todo[] = [...todos, todo]
+  fs.writeFileSync('./db.json', JSON.stringify(newTodos, null, 2))
+  res.json({ message: 'Success!' })
+})
+
+app.delete('/todos/:id', async (req, res) => {
+  if (!fs.existsSync('./db.json')) {
+    res.status(404).json({ message: 'Not found the todo.' })
+    return
+  }
+
+  const todos: Todo[] = JSON.parse(fs.readFileSync('./db.json').toString())
+  const targetTodoId = req.params.id
+  const targetTodo = todos.find((todo) => todo.id === targetTodoId)
+  if (targetTodo == null) {
+    res.status(404).json({ message: 'Not found the todo.' })
+    return
+  }
+
+  const newTodos: Todo[] = todos.filter((todo) => todo.id !== targetTodo.id)
+  fs.writeFileSync('./db.json', JSON.stringify(newTodos, null, 2))
+
+  res.json({ message: 'Success!' })
+})
+
+app.listen(3000)

6e2896d6dc6ab4504ec93bdda559a3514460c81f
Yamahitsuji <yagi.yusei@classmethod.jp>
2022-05-20 15:11:53 +0900

    adding 40-todo-e2e

--- packages/40-todo-e2e/tests/todo.test.ts
+++ packages/40-todo-e2e/tests/todo.test.ts
@@ -5,7 +5,14 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
 
 const documentClient = DynamoDBDocument.from(
-  new DynamoDBClient({ endpoint: 'http://localhost:4566' })
+  new DynamoDBClient({
+    endpoint: 'http://localhost:4566',
+    credentials: {
+      accessKeyId: 'DUMMY',
+      secretAccessKey: 'DUMMY',
+    },
+    region: 'ap-northeast-1',
+  })
 )
 
 test.beforeEach(async () => {
@@ -28,6 +35,18 @@ test('basic test', async ({ page }) => {
   await expect(page.locator('li >> nth=0')).toContainText('テスト')
 
   // DBにデータが追加されていることを確認する
+  const { Items: todos = [] } = await documentClient.scan({
+    TableName: 'todos',
+  })
+  expect(todos.length).toEqual(1)
+
   // 削除ボタンの動作と確認
+  await page.pause()
+  await page.click('text="Todo削除')
+
   // DBからデータが消されていることを確認する
+  const { Items: todosAfterDeleted = [] } = await documentClient.scan({
+    TableName: 'todos',
+  })
+  expect(todosAfterDeleted.length).toEqual(0)
 })

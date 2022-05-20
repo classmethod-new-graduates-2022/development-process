195baacc7f015269e27baddc8dce4995597dc110
Yamahitsuji <yagi.yusei@classmethod.jp>
2022-05-20 13:20:54 +0900

    adding 30-greet

--- packages/30-current-time/greet.test.ts
+++ packages/30-current-time/greet.test.ts
@@ -2,14 +2,14 @@ import { test, expect } from 'vitest'
 import { buildGreet } from './greet'
 
 test.each([
-  [new Date('2022-04-01T04:59:59+09:00'), 'こんばんは'],
-  [new Date('2022-04-01T05:00:00+09:00'), 'おはようございます'],
-  [new Date('2022-04-01T11:59:59+09:00'), 'おはようございます'],
-  [new Date('2022-04-01T12:00:00+09:00'), 'こんにちは'],
-  [new Date('2022-04-01T17:59:59+09:00'), 'こんにちは'],
-  [new Date('2022-04-01T18:00:00+09:00'), 'こんばんは'],
-])('%sの時は%s', (date, expected) => {
+  [new Date('2022-04-01T04:59:59+09:00'), 'TOM', 'こんばんはTOM'],
+  [new Date('2022-04-01T05:00:00+09:00'), 'TOM', 'おはようございますTOM'],
+  [new Date('2022-04-01T11:59:59+09:00'), 'TOM', 'おはようございますTOM'],
+  [new Date('2022-04-01T12:00:00+09:00'), 'Alice', 'こんにちはAlice'],
+  [new Date('2022-04-01T17:59:59+09:00'), 'Alice', 'こんにちはAlice'],
+  [new Date('2022-04-01T18:00:00+09:00'), 'Alice', 'こんばんはAlice'],
+])('%sに%sに挨拶をする', (date, name, expected) => {
   const nowFetcher = () => date
   const greet = buildGreet(nowFetcher)
-  expect(greet('TOM')).toBe(expected)
+  expect(greet(name)).toBe(expected)
 })
